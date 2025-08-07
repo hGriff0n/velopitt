51da2bb897371179b3c45b1de2f092a48e6ba19b
Grayson Hooper <ghooper96@gmail.com>
2025-08-07 19:45:25 -0400

    Test Out Automated Deployment

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,8 +2,9 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { APP_INITIALIZER } from '@angular/core';
+import { provideMapboxGL } from 'ngx-mapbox-gl';
 
-import { AppConfigService } from './services/config-service';
+import { ConfigService } from './services/config-service';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
@@ -18,11 +19,13 @@ import { App } from './app';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    // TODO: me - This should be in github secrets
+    provideMapboxGL({accessToken: ""}),
     {
       provide: APP_INITIALIZER,
       multi: true,
-      deps: [AppConfigService],
-      useFactory: (appConfigService: AppConfigService) => {
+      deps: [ConfigService],
+      useFactory: (appConfigService: ConfigService) => {
         return () => {
           //Make sure to return a promise!
           return appConfigService.loadAppConfig();
--- src/app/services/config-service.ts
+++ src/app/services/config-service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class AppConfigService {
+export class ConfigService {
 
   private appConfig: any;
 
