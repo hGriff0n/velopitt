947b039cbd34c33159bd9611b8455613af5f5e43
Grayson Hooper <ghooper96@gmail.com>
2026-01-10 20:29:09 -0500

    Initial Setup and Interactions with Antigravity IDE
    Going to be testing out these vibe coding workflows while I'm indisposed with surgery

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MapComponent } from 'ngx-mapbox-gl';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { App } from './app';
+import { ConfigService } from './services/config-service';
+import { SegmentService } from './services/segment-service';
+import { OverlayService } from './services/overlay-service';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        RouterModule.forRoot([]),
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MapComponent
       ],
       declarations: [
         App
       ],
+      providers: [
+        { provide: ConfigService, useValue: {} },
+        { provide: SegmentService, useValue: { getAllSegments: () => [] } },
+        { provide: OverlayService, useValue: {} }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
@@ -20,10 +40,9 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should have the title signal set to "velopitt"', () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, velopitt');
+    const app = fixture.componentInstance;
+    expect(app['title']()).toEqual('velopitt');
   });
 });
